e2f4f42ca42d84502e7cc5dc8b6a5e3b7fad4057
donchan91 <63372896+donchan91@users.noreply.github.com>
2020-08-15 13:54:08 +0100

    Add files via upload
    phq9.pdf is the questionnaire in .pdf format. It is there for reference purposes only, and has been generated from https://patient.info/doctor/patient-health-questionnaire-phq-9

---
+++ phq9.py
@@ -0,0 +1,135 @@
+from datetime import datetime
+from time import strftime
+import pandas as pd
+import numpy as np
+import csv
+# Importing os.path to for
+import os.path
+
+question_number = 1
+appname = "MH Tracker"
+now = datetime.now()
+now = now.strftime("%Y-%m-%d")
+start = True
+
+def questionnaire():
+    # Initialising variables.
+    global start
+    start = True
+    global question_number
+    score = []
+    total = 0
+    severity = ""
+    date_of_assessment = now
+    choice = "\n1. Not at all\n2. Several Days\n3.More than half the days\n4. Nearly every day.\n"
+
+    # check_ans checks if the score is in between 1 and 4, and that it is an integer
+    def check_answer(ans):
+        global question_number
+        if ans.isdigit():
+            ans = int(ans)
+            if ans >= 1 and ans <= 4:
+                score.append(ans)
+                question_number += 1
+            else:
+                print("Input invalid. Please try again.")
+                return ans
+        else:
+            print("Input invalid. Please try again.")
+            return ans
+
+    # Function to assess severity
+    def assess_severity(x):
+        if x >= 0 and x <= 4:
+            return "subclinical"
+        elif x >= 5 and x <= 9:
+            return "mild"
+        elif x >= 10 and x <= 14:
+            return "moderate"
+        elif x >= 15 and x <= 19:
+            return "moderately severe"
+        else:
+            return "severe"
+
+    # Introductory message
+    print("This assessment can help you better understand your mental well-being over a long period of time by recording answers to your questions and saving this in a savefile. The savefile can be loaded again onto this program to keep track of your progress.\nIt is advised that you do NOT answer this questionnaire more than once per day.")
+
+    while start == True:
+        # User prompt to begin
+        yn = input("Are you ready to take the questionnaire? Y/N\n").lower()
+        if yn == "y":
+            # PHQ-9 starts here if user selects "y"
+            while question_number <= 10:
+                if question_number == 1:
+                    q1 = input("Over the last two weeks, have you had little interest or pleasure in doing things?%s" % choice)
+                    check_answer(q1)
+                elif question_number == 2:
+                    q2 = input("Over the last two weeks, have you been feeling down, depressed, or hopeless?%s" % choice)
+                    check_answer(q2)
+                elif question_number == 3:
+                    q3 = input("Over the last two weeks, have you had trouble falling asleep, staying asleep or sleeping too much?%s" % choice)
+                    check_answer(q3)
+                elif question_number == 4:
+                    q4 = input("Over the last two weeks, have you been feeling tired or having little energy?%s" % choice)
+                    check_answer(q4)
+                elif question_number == 5:
+                    q5 = input("Over the last two weeks, have you had poor appetite or were over-eating?:%s" % choice)
+                    check_answer(q5)
+                elif question_number == 6:
+                    q6 = input("Over the last two weeks, have you been feeling bad about yourself - or that you are a failure or have let yourself or your family down?:%s" % choice)
+                    check_answer(q6)
+                elif question_number == 7:
+                    q7 = input("Over the last two weeks, have you had trouble concentrating on things, such as reading the newspaper or watching television?:%s" % choice)
+                    check_answer(q7)
+                elif question_number == 8:
+                    q8 = input("Over the last two weeks, have you moving or speaking so slowly that other people could have noticed? \n \
+    Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual? %s" % choice)                
+                    check_answer(q8)
+                elif question_number == 9:
+                    q9 = input("Over the last two weeks, have you had thoughts that you would be better off dead, or of hurting yourself in some way?: %s" % choice)
+                    check_answer(q9)
+                else:
+                    total = sum(score) - 9
+                    print("Thank you for answering these questions.")
+                    print("The scores you have entered were " + str(score))
+                    severity = assess_severity(total)
+                    print("Your PHQ-9 score is " + str(total) +
+                        ", which is classified by this questionnaire as " + severity + ".")
+                    go_back = input(
+                        "Press M to return to menu, X to exit program.").lower()
+                    if go_back == "m":
+                        main_screen()
+                    elif go_back == "x":
+                        start = False
+                    else:
+                        print("Please enter a valid input.")
+        elif yn == "n":
+            main_screen()
+        else:
+            print("Please enter a valid input.")
+
+
+def display_scores():
+    print("Placeholder for displaying scores")
+
+
+def main_screen():
+    global start
+    print("Today's date is %s" % now)
+    print("This is the PHQ-9 questionnaire.\nPlease choose from the following options. \nPLEASE NOTE: This is NOT used as a diagnostic tool for depression and related disorders. Should there be genuine concerns about the mental well-being of you or someone you know, you should consult a medical professional for their assessment and/or treatment.")
+    while start == True:
+        choice = input(
+            "Please choose from the following:\nA: Answer PHQ-9\nD: Display trend.\nX: Exit application.\n").lower()
+        if choice == "a":
+            questionnaire()
+            start = False
+        elif choice == "d":
+            display_scores()
+            start = False
+        elif choice == "x":
+            start = False
+        else:
+            print("Please enter a valid input.")
+
+
+main_screen()
